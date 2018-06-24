b534246e44d9ff2a47f36c968231fe004885e9c9
szepczyn <szepczyn@bitbucket.org>
2018-06-24 14:55:45 +0200

    time logic

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 //chunk: {hours, minutes, seconds}
 //5:27:53
 
+console.log('repo-ext-markers init');
+
 const extractTime = timeStr => {
   let hours;
   let minutes;
   let seconds;
   const groups = timeStr.split(":");
-  if(groups.length == 3){
+  if (groups.length == 3) {
     hours = Number(groups[0]);
     minutes = Number(groups[1]);
     seconds = Number(groups[2]);
-  }else if(groups.length == 2){
+  } else if (groups.length == 2) {
     hours = 0;
     minutes = Number(groups[0]);
     seconds = Number(groups[1]);
-  }else {
+  } else {
     throw new Error('Time group in wrong format ', timeStr);
   }
-  
+
   return {
     hours,
     minutes,
@@ -25,19 +27,16 @@ const extractTime = timeStr => {
   };
 };
 
+const createTimeStamp = secondsSum => chunk => {
+  return {
+    percentPos: toPercentTime(secondsSum)(extractTime(chunk.at)),
+    label: chunk.label 
+  }
+};
 const calcSecondsSum = ({ hours, minutes, seconds }) => seconds + minutes * 60 + hours * 3600;
 const toPercentTime = sum => chunk => {
   return calcSecondsSum(chunk) / sum;
 }
-
-console.log('autorun');
-const result = document.getElementById('description').innerText;
-console.log(result);
-
-const bar = document.getElementsByClassName('ytp-progress-bar-container')[0];
-const container = document.createElement('div');
-container.classList.add('repo-ext-yt-container');
-bar.appendChild(container);
 
 const waitMiliseconds = (ms) => {
   return new Promise(resolve => setTimeout(resolve, ms));
@@ -45,35 +44,50 @@ const waitMiliseconds = (ms) => {
 
 const waitForYt = async () => {
   let tries = 0;
-  while(true){
+  while (true) {
     await waitMiliseconds(1000);
-    if(document.getElementsByClassName('ytp-time-duration')[0]){
+    if (document.getElementsByClassName('ytp-time-duration')[0]) {
       break;
     }
     tries++;
-    if(tries > 5) {
+    if (tries > 5) {
       throw new ('Too many tries, no element present');
     }
   }
+};
+
+const appenMarks = chunks => {
+  const bar = document.getElementsByClassName('ytp-progress-bar-container')[0];
+  const container = document.createElement('div');
+  container.classList.add('repo-ext-yt-container');
+  bar.appendChild(container);
 };
 
 waitForYt().then(() => {
   const durationStr = document.getElementsByClassName('ytp-time-duration')[0].innerText;
 
 
-const {hours, minutes, seconds} = extractTime(durationStr);
+  const { hours, minutes, seconds } = extractTime(durationStr);
 
 
-console.log('time', hours, minutes, seconds);
+  console.log('time', hours, minutes, seconds);
 
-const secondsSum = calcSecondsSum({
-  hours,
-  minutes,
-  seconds
-});
-const chunksStrings = ['2:40:00'];
+  const secondsSum = calcSecondsSum({
+    hours,
+    minutes,
+    seconds
+  });
+  // TODO extract chunks from description
+  const desc = document.getElementById('description').innerText;
+  console.log(desc);
+  const chunksStrings = [{
+    at: '2:40:00',
+    label: 'Sample label' 
+  }];
 
-const percentChunks = chunksStrings.map(extractTime).map(toPercentTime(secondsSum));
-console.log('chunks', percentChunks);
+  const percentChunks = chunksStrings.map(createTimeStamp(secondsSum));
+  console.log('chunks', percentChunks);
+
+  appendMarks(percentChunks);
 });
 
